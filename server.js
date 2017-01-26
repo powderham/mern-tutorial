f8881f812eeacfc77b45a841042a1fc10e101523
Edward Powderham <edpowderham@outlook.com>
2017-01-26 16:40:08 +0000

    Adding API routing

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 import config from './config';
 import express from 'express';
-import fs from 'fs';
+import apiRouter from './api';
 const server = express();
 
 server.get('/', (req, res) => {
   res.send("Working!")
 });
 
-// Before using the express middleware, this is how you make a route
-server.get('/about.html', (req, res) => {
-  fs.readFile('./about.html', (err, data) => {
-    res.send(data.toString());
-  });
-});
+// calling the api '/' routing from /api
+server.use('/api', apiRouter);
+
+server.use(express.static('public'));
 
 server.listen(config.port, () => {
   console.info('Express listening on port: ', config.port);
